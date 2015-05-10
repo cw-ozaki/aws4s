bd6ae3dcf43b74a48b143ece7bbddf26673f6bb4
Junichi Kato <j5ik2o@gmail.com>
2015-05-10 16:11:02 +0900

    Refactor.

--- project/Build.scala
+++ project/Build.scala
@@ -57,57 +57,63 @@ object ApplicationBuild extends Build {
     }
   )
 
-  lazy val root = Project(id = "aws4s",
-    base = file(".")).settings(commonSettings: _*)
+  lazy val root = Project(id = "aws4s", base = file(".")).settings(commonSettings: _*)
     .aggregate(awsCore, awsDynamoDB, awsDynamoDBExt, awsS3, awsSQS, awsElasticBeanstalk)
 
-  lazy val awsCore = Project(id = "aws4s-core", base = file("aws4s-core")).
-    settings(commonSettings: _*)
+  lazy val awsCore = Project(id = "aws4s-core", base = file("aws4s-core"))
+    .settings(commonSettings: _*)
 
-  lazy val awsSQS = Project(id = "aws4s-sqs", base = file("aws4s-sqs")).
-    settings(commonSettings: _*).dependsOn(awsCore).settings(
+  lazy val awsSQS = Project(id = "aws4s-sqs", base = file("aws4s-sqs"))
+    .settings(commonSettings: _*).dependsOn(awsCore).settings(
       libraryDependencies ++= Seq(
         "com.amazonaws" % "aws-java-sdk-sqs" % awsSdkVersion
       )
     )
 
-  lazy val awsS3 = Project(id = "aws4s-s3", base = file("aws4s-s3")).
-    settings(commonSettings: _*).dependsOn(awsCore).settings(
+  lazy val awsS3 = Project(id = "aws4s-s3", base = file("aws4s-s3"))
+    .settings(commonSettings: _*).dependsOn(awsCore).settings(
       libraryDependencies ++= Seq(
         "com.amazonaws" % "aws-java-sdk-s3" % awsSdkVersion
       )
     )
 
-  lazy val awsDynamoDBExt = Project(
-    id = "aws4s-dynamodb-ext",
-    base = file("aws4s-dynamodb-ext")
-  ).settings(commonSettings: _*).dependsOn(awsDynamoDB)
+  lazy val awsDynamoDBExt = Project(id = "aws4s-dynamodb-ext", base = file("aws4s-dynamodb-ext"))
+    .settings(commonSettings: _*).dependsOn(awsDynamoDB)
 
-  lazy val awsDynamoDB = Project(id = "aws4s-dynamodb",
-    base = file("aws4s-dynamodb")).
-    settings(commonSettings: _*).dependsOn(awsCore).settings(
+  lazy val jettyDependencies = Seq(
+    "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+    "org.antlr" % "antlr4-runtime" % "4.1" % "test",
+    "commons-cli" % "commons-cli" % "1.2" % "test",
+    "com.fasterxml.jackson.core" % "jackson-core" % "2.1.1" % "test",
+    "com.fasterxml.jackson.core" % "jackson-annotations" % "2.1.1" % "test",
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.1.1" % "test",
+    "joda-time" % "joda-time" % "2.2" % "test",
+    "log4j" % "log4j" % "1.2.17" % "test",
+    "javax.servlet" % "javax.servlet-api" % "3.0.1" % "test",
+    "org.eclipse.jetty.spdy" % "spdy-core" % "8.1.12.v20130726" % "test",
+    "org.eclipse.jetty.spdy" % "spdy-jetty" % "8.1.12.v20130726" % "test",
+    "org.eclipse.jetty.spdy" % "spdy-jetty-http" % "8.1.12.v20130726" % "test",
+    "org.eclipse.jetty" % "jetty-server" % "8.1.12.v20130726" % "test"
+  )
+
+  lazy val awsDynamoDB = Project(id = "aws4s-dynamodb", base = file("aws4s-dynamodb"))
+    .settings(commonSettings: _*).dependsOn(awsCore).settings(
       libraryDependencies ++= Seq(
-        "com.amazonaws" % "aws-java-sdk-dynamodb" % awsSdkVersion,
-        "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-        "org.antlr" % "antlr4-runtime" % "4.1" % "test",
-        "commons-cli" % "commons-cli" % "1.2" % "test",
-        "com.fasterxml.jackson.core" % "jackson-core" % "2.1.1" % "test",
-        "com.fasterxml.jackson.core" % "jackson-annotations" % "2.1.1" % "test",
-        "com.fasterxml.jackson.core" % "jackson-databind" % "2.1.1" % "test",
-        "joda-time" % "joda-time" % "2.2" % "test",
-        "log4j" % "log4j" % "1.2.17" % "test",
-        "javax.servlet" % "javax.servlet-api" % "3.0.1" % "test",
-        "org.eclipse.jetty.spdy" % "spdy-core" % "8.1.12.v20130726" % "test",
-        "org.eclipse.jetty.spdy" % "spdy-jetty" % "8.1.12.v20130726" % "test",
-        "org.eclipse.jetty.spdy" % "spdy-jetty-http" % "8.1.12.v20130726" % "test",
-        "org.eclipse.jetty" % "jetty-server" % "8.1.12.v20130726" % "test"
+        "com.amazonaws" % "aws-java-sdk-dynamodb" % awsSdkVersion
+      ) ++ jettyDependencies
+    )
+
+  lazy val awsElasticBeanstalk = Project(id = "aws4s-eb", base = file("aws4s-eb"))
+    .settings(commonSettings: _*).dependsOn(awsCore).settings(
+      libraryDependencies ++= Seq(
+        "com.amazonaws" % "aws-java-sdk-elasticbeanstalk" % awsSdkVersion
       )
     )
 
-  lazy val awsElasticBeanstalk = Project(id = "aws4s-eb", base = file("aws4s-eb")).
-    settings(commonSettings: _*).dependsOn(awsCore).settings(
+  lazy val awsCloudFormation = Project(id = "aws4s-cfn", base = file("aws4s-cfn"))
+    .settings(commonSettings: _*).dependsOn(awsCore).settings(
       libraryDependencies ++= Seq(
-        "com.amazonaws" % "aws-java-sdk-elasticbeanstalk" % awsSdkVersion
+        "com.amazonaws" % "aws-java-sdk-cloudformation" % awsSdkVersion
       )
     )
 
