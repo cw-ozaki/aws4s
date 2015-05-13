55ba1469e64f9027a72698237981a10819d5c72a
Junichi Kato <j5ik2o@gmail.com>
2015-05-13 12:32:11 +0900

    Refactor

--- aws4s-dynamodb/src/main/scala/org/sisioh/aws4s/dynamodb/Implicits.scala
+++ aws4s-dynamodb/src/main/scala/org/sisioh/aws4s/dynamodb/Implicits.scala
@@ -2,16 +2,14 @@ package org.sisioh.aws4s.dynamodb
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document._
-import com.amazonaws.services.dynamodbv2.document.internal.{ Filter, PageBasedCollection }
+import com.amazonaws.services.dynamodbv2.document.internal.{Filter, PageBasedCollection}
 import com.amazonaws.services.dynamodbv2.model._
 import org.sisioh.aws4s.dynamodb.document._
 import org.sisioh.aws4s.dynamodb.model._
 
 object Implicits extends Implicits
 
-trait Implicits extends AmazonDynamoDBClientImplicits with DocumentImplicits with ModelImplicits
-
-trait AmazonDynamoDBClientImplicits {
+trait Implicits extends DocumentImplicits with ModelImplicits {
 
   implicit def richAmazonDynamoDBClient(underlying: AmazonDynamoDBClient): RichAmazonDynamoDBClient = new RichAmazonDynamoDBClient(underlying)
 
@@ -72,53 +70,11 @@ trait DocumentImplicits {
 
 trait ModelImplicits {
 
-  // -- LocalSecondaryIndex
+  implicit def richAttributeDefinition(underlying: AttributeDefinition): RichAttributeDefinition = new RichAttributeDefinition(underlying)
 
-  implicit def richLocalSecondaryIndex(underlying: LocalSecondaryIndex): RichLocalSecondaryIndex = new RichLocalSecondaryIndex(underlying)
+  implicit def richAttributeValue(underlying: AttributeValue): RichAttributeValue = new RichAttributeValue(underlying)
 
-  implicit def richLocalSecondaryIndexDescription(underlying: LocalSecondaryIndexDescription): RichLocalSecondaryIndexDescription = new RichLocalSecondaryIndexDescription(underlying)
-
-  // -- GlobalSecondaryIndex
-
-  implicit def richGlobalSecondaryIndex(underlying: GlobalSecondaryIndex): RichGlobalSecondaryIndex = new RichGlobalSecondaryIndex(underlying)
-
-  implicit def richGlobalSecondaryIndexDescription(underlying: GlobalSecondaryIndexDescription): RichGlobalSecondaryIndexDescription = new RichGlobalSecondaryIndexDescription(underlying)
-
-  implicit def richGlobalSecondaryIndexUpdate(underlying: GlobalSecondaryIndexUpdate): RichGlobalSecondaryIndexUpdate = new RichGlobalSecondaryIndexUpdate(underlying)
-
-  implicit def richUpdateGlobalSecondaryIndexAction(underlying: UpdateGlobalSecondaryIndexAction): RichUpdateGlobalSecondaryIndexAction = new RichUpdateGlobalSecondaryIndexAction(underlying)
-
-  // -- Query/Scan
-
-  implicit def richQueryRequest(underlying: QueryRequest): RichQueryRequest = new RichQueryRequest(underlying)
-
-  implicit def richQueryResult(underlying: QueryResult): RichQueryResult = new RichQueryResult(underlying)
-
-  implicit def richScanRequest(underlying: ScanRequest): RichScanRequest = new RichScanRequest(underlying)
-
-  implicit def richScanResult(underlying: ScanResult): RichScanResult = new RichScanResult(underlying)
-
-  implicit def richCondition(underlying: Condition): RichCondition = new RichCondition(underlying)
-
-  // -- Item
-
-  implicit def richPutItemRequest(underlying: PutItemRequest): RichPutItemRequest = new RichPutItemRequest(underlying)
-
-  implicit def richPutItemResult(underlying: PutItemResult): RichPutItemResult = new RichPutItemResult(underlying)
-
-  implicit def richGetItemRequest(underlying: GetItemRequest): RichGetItemRequest = new RichGetItemRequest(underlying)
-
-  implicit def richGetItemResult(underlying: GetItemResult): RichGetItemResult = new RichGetItemResult(underlying)
-
-  implicit def richUpdateItemRequest(underlying: UpdateItemRequest): RichUpdateItemRequest = new RichUpdateItemRequest(underlying)
-
-  implicit def richUpdateItemResult(underlying: UpdateItemResult): RichUpdateItemResult = new RichUpdateItemResult(underlying)
-
-  implicit def richDeleteItemRequest(underlying: DeleteItemRequest): RichDeleteItemRequest = new RichDeleteItemRequest(underlying)
-
-  implicit def richDeleteItemResult(underlying: DeleteItemResult): RichDeleteItemResult = new RichDeleteItemResult(underlying)
-
-  // -- Batch Item
+  implicit def richAttributeValueUpdate(underlying: AttributeValueUpdate): RichAttributeValueUpdate = new RichAttributeValueUpdate(underlying)
 
   implicit def richBatchGetItemRequest(underlying: BatchGetItemRequest): RichBatchGetItemRequest = new RichBatchGetItemRequest(underlying)
 
@@ -128,56 +84,92 @@ trait ModelImplicits {
 
   implicit def richBatchWriteItemResult(underlying: BatchWriteItemResult): RichBatchWriteItemResult = new RichBatchWriteItemResult(underlying)
 
-  // -- Table
+  implicit def richCapacity(underlying: Capacity): RichCapacity = new RichCapacity(underlying)
+
+  implicit def richCondition(underlying: Condition): RichCondition = new RichCondition(underlying)
+
+  implicit def richConsumedCapacity(underlying: ConsumedCapacity): RichConsumedCapacity = new RichConsumedCapacity(underlying)
+
+  implicit def richCreateGlobalSecondaryIndexAction(underlying: CreateGlobalSecondaryIndexAction): RichCreateGlobalSecondaryIndexAction = new RichCreateGlobalSecondaryIndexAction(underlying)
 
   implicit def richCreateTableRequest(underlying: CreateTableRequest): RichCreateTableRequest = new RichCreateTableRequest(underlying)
 
   implicit def richCreateTableResult(underlying: CreateTableResult): RichCreateTableResult = new RichCreateTableResult(underlying)
 
-  implicit def richListTablesResult(underlying: ListTablesResult): RichListTablesResult = new RichListTablesResult(underlying)
+  implicit def richDeleteGlobalSecondaryIndexAction(underlying: DeleteGlobalSecondaryIndexAction): RichDeleteGlobalSecondaryIndexAction = new RichDeleteGlobalSecondaryIndexAction(underlying)
 
-  implicit def richDescribeTableRequest(underlying: DescribeTableRequest): RichDescribeTableRequest = new RichDescribeTableRequest(underlying)
+  implicit def richDeleteItemRequest(underlying: DeleteItemRequest): RichDeleteItemRequest = new RichDeleteItemRequest(underlying)
 
-  implicit def richDescribeTableResult(underlying: DescribeTableResult): RichDescribeTableResult = new RichDescribeTableResult(underlying)
+  implicit def richDeleteItemResult(underlying: DeleteItemResult): RichDeleteItemResult = new RichDeleteItemResult(underlying)
 
-  implicit def richUpdateTableRequest(underlying: UpdateTableRequest): RichUpdateTableRequest = new RichUpdateTableRequest(underlying)
-
-  implicit def richUpdateTableResult(underlying: UpdateTableResult): RichUpdateTableResult = new RichUpdateTableResult(underlying)
+  implicit def richDeleteRequest(underlying: DeleteRequest): RichDeleteRequest = new RichDeleteRequest(underlying)
 
   implicit def richDeleteTableRequest(underlying: DeleteTableRequest): RichDeleteTableRequest = new RichDeleteTableRequest(underlying)
 
   implicit def richDeleteTableResult(underlying: DeleteTableResult): RichDeleteTableResult = new RichDeleteTableResult(underlying)
 
-  // -- Other
+  implicit def richDescribeTableRequest(underlying: DescribeTableRequest): RichDescribeTableRequest = new RichDescribeTableRequest(underlying)
 
-  implicit def richKeySchemaElement(underlying: KeySchemaElement): RichKeySchemaElement = new RichKeySchemaElement(underlying)
+  implicit def richDescribeTableResult(underlying: DescribeTableResult): RichDescribeTableResult = new RichDescribeTableResult(underlying)
 
-  implicit def richTableDescription(underlying: TableDescription): RichTableDescription = new RichTableDescription(underlying)
+  implicit def richExpectedAttributeValue(underlying: ExpectedAttributeValue): RichExpectedAttributeValue = new RichExpectedAttributeValue(underlying)
 
-  implicit def richAttributeDefinition(underlying: AttributeDefinition): RichAttributeDefinition = new RichAttributeDefinition(underlying)
+  implicit def richGetItemRequest(underlying: GetItemRequest): RichGetItemRequest = new RichGetItemRequest(underlying)
 
-  implicit def richAttributeValue(underlying: AttributeValue): RichAttributeValue = new RichAttributeValue(underlying)
+  implicit def richGetItemResult(underlying: GetItemResult): RichGetItemResult = new RichGetItemResult(underlying)
 
-  implicit def richAttributeValueUpdate(underlying: AttributeValueUpdate): RichAttributeValueUpdate = new RichAttributeValueUpdate(underlying)
+  implicit def richGlobalSecondaryIndex(underlying: GlobalSecondaryIndex): RichGlobalSecondaryIndex = new RichGlobalSecondaryIndex(underlying)
 
-  implicit def richConsumedCapacity(underlying: ConsumedCapacity): RichConsumedCapacity = new RichConsumedCapacity(underlying)
+  implicit def richGlobalSecondaryIndexDescription(underlying: GlobalSecondaryIndexDescription): RichGlobalSecondaryIndexDescription = new RichGlobalSecondaryIndexDescription(underlying)
 
-  implicit def richProvisionedThroughputDescription(underlying: ProvisionedThroughputDescription): RichProvisionedThroughputDescription = new RichProvisionedThroughputDescription(underlying)
-
-  implicit def richProvisionedThroughput(underlying: ProvisionedThroughput): RichProvisionedThroughput = new RichProvisionedThroughput(underlying)
-
-  implicit def richProjection(underlying: Projection): RichProjection = new RichProjection(underlying)
-
-  implicit def richWriteRequest(underlying: WriteRequest): RichWriteRequest = new RichWriteRequest(underlying)
-
-  implicit def richPutRequest(underlying: PutRequest): RichPutRequest = new RichPutRequest(underlying)
-
-  implicit def richDeleteRequest(underlying: DeleteRequest): RichDeleteRequest = new RichDeleteRequest(underlying)
+  implicit def richGlobalSecondaryIndexUpdate(underlying: GlobalSecondaryIndexUpdate): RichGlobalSecondaryIndexUpdate = new RichGlobalSecondaryIndexUpdate(underlying)
 
   implicit def richItemCollectionMetrics(underlying: ItemCollectionMetrics): RichItemCollectionMetrics = new RichItemCollectionMetrics(underlying)
 
-  implicit def richCapacity(underlying: Capacity): RichCapacity = new RichCapacity(underlying)
-
   implicit def richKeysAndAttributes(underlying: KeysAndAttributes): RichKeysAndAttributes = new RichKeysAndAttributes(underlying)
+
+  implicit def richKeySchemaElement(underlying: KeySchemaElement): RichKeySchemaElement = new RichKeySchemaElement(underlying)
+
+  implicit def richListTablesRequest(underlying: ListTablesRequest): RichListTablesRequest = new RichListTablesRequest(underlying)
+
+  implicit def richListTablesResult(underlying: ListTablesResult): RichListTablesResult = new RichListTablesResult(underlying)
+
+  implicit def richLocalSecondaryIndex(underlying: LocalSecondaryIndex): RichLocalSecondaryIndex = new RichLocalSecondaryIndex(underlying)
+
+  implicit def richLocalSecondaryIndexDescription(underlying: LocalSecondaryIndexDescription): RichLocalSecondaryIndexDescription = new RichLocalSecondaryIndexDescription(underlying)
+
+  implicit def richProjection(underlying: Projection): RichProjection = new RichProjection(underlying)
+
+  implicit def richProvisionedThroughput(underlying: ProvisionedThroughput): RichProvisionedThroughput = new RichProvisionedThroughput(underlying)
+
+  implicit def richProvisionedThroughputDescription(underlying: ProvisionedThroughputDescription): RichProvisionedThroughputDescription = new RichProvisionedThroughputDescription(underlying)
+
+  implicit def richPutItemRequest(underlying: PutItemRequest): RichPutItemRequest = new RichPutItemRequest(underlying)
+
+  implicit def richPutItemResult(underlying: PutItemResult): RichPutItemResult = new RichPutItemResult(underlying)
+
+  implicit def richPutRequest(underlying: PutRequest): RichPutRequest = new RichPutRequest(underlying)
+
+  implicit def richQueryRequest(underlying: QueryRequest): RichQueryRequest = new RichQueryRequest(underlying)
+
+  implicit def richQueryResult(underlying: QueryResult): RichQueryResult = new RichQueryResult(underlying)
+
+  implicit def richScanRequest(underlying: ScanRequest): RichScanRequest = new RichScanRequest(underlying)
+
+  implicit def richScanResult(underlying: ScanResult): RichScanResult = new RichScanResult(underlying)
+
+  implicit def richTableDescription(underlying: TableDescription): RichTableDescription = new RichTableDescription(underlying)
+
+  implicit def richUpdateGlobalSecondaryIndexAction(underlying: UpdateGlobalSecondaryIndexAction): RichUpdateGlobalSecondaryIndexAction = new RichUpdateGlobalSecondaryIndexAction(underlying)
+
+  implicit def richUpdateItemRequest(underlying: UpdateItemRequest): RichUpdateItemRequest = new RichUpdateItemRequest(underlying)
+
+  implicit def richUpdateItemResult(underlying: UpdateItemResult): RichUpdateItemResult = new RichUpdateItemResult(underlying)
+
+  implicit def richUpdateTableRequest(underlying: UpdateTableRequest): RichUpdateTableRequest = new RichUpdateTableRequest(underlying)
+
+  implicit def richUpdateTableResult(underlying: UpdateTableResult): RichUpdateTableResult = new RichUpdateTableResult(underlying)
+
+  implicit def richWriteRequest(underlying: WriteRequest): RichWriteRequest = new RichWriteRequest(underlying)
 
 }
