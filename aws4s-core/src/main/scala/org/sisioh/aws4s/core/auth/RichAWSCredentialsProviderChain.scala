2d02160ce28ecbd9650ce59126b5a69992525324
cw-ozaki <ozaki@chatwork.co.jp>
2015-10-27 19:39:23 +0900

    add factories

--- aws4s-core/src/main/scala/org/sisioh/aws4s/core/auth/RichAWSCredentialsProviderChain.scala
+++ aws4s-core/src/main/scala/org/sisioh/aws4s/core/auth/RichAWSCredentialsProviderChain.scala
@@ -1,7 +1,13 @@
 package org.sisioh.aws4s.core.auth
 
-import com.amazonaws.auth.{ AWSCredentials, AWSCredentialsProviderChain }
+import com.amazonaws.auth.{ AWSCredentials, AWSCredentialsProvider, AWSCredentialsProviderChain }
 import org.sisioh.aws4s.PimpedType
+
+object AWSCredentialsProviderChainFactory {
+
+  def createCredentialsProviders(providers: AWSCredentialsProvider*): AWSCredentialsProviderChain = new AWSCredentialsProviderChain(providers: _*)
+
+}
 
 class RichAWSCredentialsProviderChain(val underlying: AWSCredentialsProviderChain)
     extends AnyVal with PimpedType[AWSCredentialsProviderChain] {
--- aws4s-sts/src/main/scala/org/sisioh/aws4s/sts/auth/RichSTSAssumeRoleSessionCredentialsProvider.scala
+++ aws4s-sts/src/main/scala/org/sisioh/aws4s/sts/auth/RichSTSAssumeRoleSessionCredentialsProvider.scala
@@ -1,7 +1,22 @@
 package org.sisioh.aws4s.sts.auth
 
-import com.amazonaws.auth.{ AWSSessionCredentials, STSAssumeRoleSessionCredentialsProvider }
+import com.amazonaws.ClientConfiguration
+import com.amazonaws.auth.{ AWSCredentials, AWSCredentialsProvider, AWSSessionCredentials, STSAssumeRoleSessionCredentialsProvider }
 import org.sisioh.aws4s.PimpedType
+
+object STSAssumeRoleSessionCredentialsProviderFactory {
+
+  def createCredentialsProvider(longLivedCredentialsProvider: AWSCredentialsProvider, roleArn: String, roleSessionName: String) = new STSAssumeRoleSessionCredentialsProvider(longLivedCredentialsProvider, roleArn, roleSessionName)
+
+  def createCredentialsProvider(longLivedCredentialsProvider: AWSCredentialsProvider, roleArn: String, roleSessionName: String, clientConfiguration: ClientConfiguration) = new STSAssumeRoleSessionCredentialsProvider(longLivedCredentialsProvider, roleArn, roleSessionName, clientConfiguration)
+
+  def createCredentials(longLivedCredentials: AWSCredentials, roleArn: String, roleSessionName: String) = new STSAssumeRoleSessionCredentialsProvider(longLivedCredentials, roleArn, roleSessionName)
+
+  def createCredentials(longLivedCredentials: AWSCredentials, roleArn: String, roleSessionName: String, clientConfiguration: ClientConfiguration) = new STSAssumeRoleSessionCredentialsProvider(longLivedCredentials, roleArn, roleSessionName, clientConfiguration)
+
+  def createBasic(roleArn: String, roleSessionName: String): STSAssumeRoleSessionCredentialsProvider = new STSAssumeRoleSessionCredentialsProvider(roleArn, roleSessionName)
+
+}
 
 class RichSTSAssumeRoleSessionCredentialsProvider(val underlying: STSAssumeRoleSessionCredentialsProvider)
     extends AnyVal with PimpedType[STSAssumeRoleSessionCredentialsProvider] {
